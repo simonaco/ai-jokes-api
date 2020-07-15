96c87a127bfc15c5dba9f8fa33a8bdaf949c8e25
Simona Cotin <simona.cotin@gmail.com>
2020-07-15 06:36:58 +0100

    change dependencies

--- generate-joke/__init__.py
+++ generate-joke/__init__.py
@@ -12,10 +12,10 @@ import json
 
 def write_http_response(status, body_dict):
     return_dict = {
-        "status": status,
-        "body": json.dumps(body_dict),
-        "headers": {
-            "Content-Type": "application/json"
+        'status': status,
+        'body': json.dumps(body_dict),
+        'headers': {
+            'Content-Type': 'application/json'
         }
     }
     return json.dumps(return_dict)
@@ -24,12 +24,11 @@ def write_http_response(status, body_dict):
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     tempFilePath = tempfile.gettempdir()
-    jokesfile = os.path.join(tempFilePath, "jokes.hdf5")
+    jokesfile = os.path.join(tempFilePath, 'jokes.hdf5')
     urllib.request.urlretrieve(
-        "https://trickters.blob.core.windows.net/jokes/jokes.hdf5", jokesfile)
+        'https://trickters.blob.core.windows.net/jokes/jokes.hdf5', jokesfile)
     textgen = textgenrnn(jokesfile)
     joke = textgen.generate(return_as_list=True)[0]
-    logging.info(f"hello from github action joke: {joke}")
     return write_http_response(
         200,
         {'joke': joke}
